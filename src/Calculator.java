3b249932c44408c58d609bb34ef4538c41208c59
Kushtarbek <dr.kushtarbek@gmail.com>
2019-01-21 15:28:45 -0600

    Add minus method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,12 @@
 public class Calculator {
 
+public static double add(double num1, double num2){
+    return num1+num2;
+}
 
+public static double minus(double num1, double num2){
+    return num1-num2;
+}
 
 
 
