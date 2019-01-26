789b280d95563370895d55f49859490d0527a394
Kushtarbek <dr.kushtarbek@gmail.com>
2019-01-25 18:28:23 -0600

    adding recent changes

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,7 +5,8 @@ public static double add(double num1, double num2){
 }
 
 public static double minus(double num1, double num2){
-    return num1-num2;
+    double sum = num1-num2;
+    return sum;
 }
 
 
